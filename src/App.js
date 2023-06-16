8d58c5536455aa96091f004c6f41eb93e3c49bae
unbearabletruth <antaarty@gmail.com>
2023-06-16 17:37:59 +0700

    handle outside click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './assets/App.css';
 import picture from './assets/picture.jpg'
 import waldo from './assets/waldo.png'
@@ -9,6 +9,19 @@ function App() {
   const [clicked, setClicked] = useState(false);
   const [xCoord, setXcoord] = useState(0);
   const [yCoord, setYcoord] = useState(0);
+  const gamePicture = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (gamePicture.current && gamePicture.current !== e.target) {
+        setClicked(false)
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [gamePicture]);
 
   const onClick = (e) => {
     setClicked(true)
@@ -17,15 +30,45 @@ function App() {
   }
 
   return (
-    <>
-    <div>
-      <img src={waldo} alt="Waldo"></img>
-      <img src={odlaw} alt="Odlaw"></img>
-      <img src={wizard} alt="Wizard"></img>
-    </div>
-    <div>
-    {clicked ? 
-        <>
+    <div className="App">
+      <div>
+        <img src={waldo} alt="Waldo"></img>
+        <img src={odlaw} alt="Odlaw"></img>
+        <img src={wizard} alt="Wizard"></img>
+      </div>
+      <div>
+      {clicked ? 
+          <>
+            <div className="gamePictureWrapper">
+              <img 
+                ref={gamePicture}
+                id="gamePicture"
+                src={picture} 
+                alt="where's waldo?"
+                onClick={onClick}
+              >
+              </img>
+              <div 
+                id='dropDownMenu'
+                style={{left: xCoord - 30, top: yCoord - 30}}
+              >
+                <div id="targetBox">
+                </div>
+                <div className='variants'>
+                  <div id="Waldo">
+                    Waldo
+                  </div>
+                  <div id="Wilma">
+                    Wilma
+                  </div>
+                  <div id="Wizard">
+                    Wizard
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        :
           <div className="App">
             <img 
               src={picture} 
@@ -34,37 +77,9 @@ function App() {
             >
             </img>
           </div>
-          <div 
-            id='dropDownMenu'
-            style={{left: xCoord - 30, top: yCoord - 30}}
-          >
-            <div id="targetBox">
-            </div>
-            <div className='variants'>
-              <div id="Waldo">
-                Waldo
-              </div>
-              <div id="Wilma">
-                Wilma
-              </div>
-              <div id="Wizard">
-                Wizard
-              </div>
-            </div>
-          </div>
-        </>
-      :
-        <div className="App">
-          <img 
-            src={picture} 
-            alt="where's waldo?"
-            onClick={onClick}
-          >
-          </img>
-        </div>
-    }
+      }
+      </div>
     </div>
-    </>
   );
 }
 
