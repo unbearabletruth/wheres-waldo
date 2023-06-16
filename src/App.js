7f2534492346db2c987482a43c16fb85287addd2
unbearabletruth <antaarty@gmail.com>
2023-06-16 12:57:21 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import waldo from './assets/waldo.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={waldo} alt="where's waldo?"></img>
     </div>
   );
 }
